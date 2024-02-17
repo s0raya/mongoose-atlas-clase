c40da34c31317e7acd5ab5187372fefffa30fe24
s0raya <soraya.alcobet@gmail.com>
2024-02-17 21:56:17 +0100

    "complete routes"

--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,35 @@ router.post('/create', async(req, res) => {
     }
 }),
 
+router.get("/", async(req, res) => {
+    try {
+        const users = await User.find();
+        res.send(users);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+router.put("/id/:_id", async(req, res) => {
+        try {
+            const user = await User.findByIdAndUpdate(req.params._id, req.body, { new: true })
+            res.send({ message: "user successfully updated", user });
+        } catch (error) {
+            console.error(error);
+        }
+});
+
+router.delete("/id/:_id", async(req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params._id);
+        res.send({ message: "user deleted", user });
+    } catch (error) {
+        console.error(error);
+        res
+            .status(500)
+            .send({ message: "There was a problem trying to delete a user" });
+    }
+});
+
+
 module.exports = router;
